49e1b8f57a26bfa0d11243100bfa3fd7324fd63a
Christopher Goes <ghostofgoes@gmail.com>
2016-03-20 23:33:29 -0700

    input parsing working + pycharm setup

--- hw4/id3.py
+++ hw4/id3.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python3
 
-print("test")
+import fileinput
+
+
+
+
+def calc_entropy(p):
+    if p != 0:
+        return -p * p.log2(p) # book had "np.log2(p)", maybe a typo?
+    else:
+        return 0
+
+
+
+
+
+
+
+input_debugging = True
+features = []
+answers = []
+
+# Input the file
+for line in fileinput.input():
+    linenum = fileinput.lineno()
+    if linenum == 1:
+        num_features = int(line)
+    elif linenum <= num_features + 1:
+        features.append(line.split())
+    elif linenum == num_features + 2:
+        possible_answers = line.split()
+    else:
+        answers.append(line.split())
+
+
+if input_debugging:
+    print("num_features: " + str(num_features))
+    for feature in features:
+        print(feature)
+
+    print("possible_answers: " + str(possible_answers))
+    for answer in answers:
+        print(answer)
+
+
