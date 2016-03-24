856bf47e8bfb0d5e1c66fb20c762884f87fec712
Christopher Goes <goes.chris@gmail.com>
2016-03-24 14:31:41 -0700

    made a pointlessly advanced testscript

--- hw4/test4.sh
+++ hw4/test4.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-test=$1
+suite="$1"
+test="$2"
 
-python3 id3.py < ./testDataA4/$test.in
+if [ $suite -eq "1" ] 
+then path="./data/";
+fi
+
+if [ $suite -eq "2" ] 
+then path="./v2testdata/";
+fi
+
+if [ $test == "all" ] 
+then for f in $path*.in; do python3 id3.py "$f"; done
+else python3 id3.py < ./testDataA4/$test.in;
+fi
+
