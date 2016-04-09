bade6e7bc7db56dde32ac9da7ed2c0f4c9561ee8
Christopher Goes <ghostofgoes@gmail.com>
2016-04-09 13:01:59 -0700

    correlation and eigen working

--- hw5/pca.cpp
+++ hw5/pca.cpp
@@ -6,14 +6,10 @@
 // Github:		https://github.com/GhostofGoes/cgoes-cs404
 
 #include <iostream>
-//#include <iomanip>
-//#include <cstdlib>
-//#include <vector>
-//#include <cmath>
-//#include "matrix.h"
 #include "mat.h"
 
-#define DEBUG 0
+#define DEBUG 1
+#define DEBUG_EIGEN 1
 #define DEBUGINPUT 0
 #define DEBUG_RESULTS 0
 
@@ -21,23 +17,30 @@ using namespace std;
 
 int main() {
 	
-	// Initialization
 	int numRows = 0;
 	int numCols = 0;
-	int totalDatapoints = 0;
-	double temp = 0;
-	
-	//	Data Input 	//
-	//	Input data into a matrix
 	Matrix data("data");
-	data.read();
 	
-	if(DEBUGINPUT)
-	{
-		cout << "**data matrix**" << endl;
-		data.print();
-		//inputData.printAll();
-	}
+	// Read in the input dataset
+	data.read();
+	if(DEBUGINPUT) { cout << "**data matrix**" << endl; data.print(); }
+	
+	numRows = data.maxRows();
+	numCols = data.maxCols();
+	if(DEBUGINPUT) { cout << "numRows: " << numRows << endl; cout << "numCols: " << numCols << endl; }
+	
+	// Correlation Matrix
+	Matrix corr(data.cov());
+	if(DEBUG) { cout << "\nCorrelation Matrix" << endl; corr.print(); }
+
+	// Create eigen vectors and values
+	Matrix eVecs(corr);
+	Matrix eVals(eVecs.eigenSystem());
+	if(DEBUG_EIGEN) { cout << "\nEigen Vectors" << endl; eVecs.print(); }
+	if(DEBUG_EIGEN) { cout << "\nEigen Values" << endl; eVals.print(); }
+	
+	eVecs.normalize();
+	if(DEBUG_EIGEN) { cout << "\nNormalized Eigen Vectors" << endl; eVecs.print(); }
 	
 	return(0);
 }
