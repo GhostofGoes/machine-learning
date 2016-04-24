7186b28f9d4066ed16602f274204dd7d17563a15
Christopher Goes <ghostofgoes@gmail.com>
2016-04-24 04:15:25 +0000

    now there is Q and R lol

--- hw6/ql.cpp
+++ hw6/ql.cpp
@@ -17,37 +17,53 @@ using namespace std;
 // action(t) = 1 forward, action(t) = 0 coast, action(t) = -1 reverse
 // bound enforces −1.2 ≤ xt+1 ≤ 0.8 and −0.07 ≤ vt+1 ≤ 0.07
 
+int next( double pos, double vel, double action );
 double v( double currVel, double action, double currPos ); 	// vt+1 = bound(vt + 0.001at − 0.0025 cos(3xt))
 double x( double currPos, double newVel ); 					// xt+1 = bound(xt + vt+1)
 double clampX( double x );
 double clampY( double y );
+Matrix r[3]; // reward matrix that has our "environment" state
+Matrix q[3]; // path matrix
 
 int main() {
 	
-	int goal = 0.8;		// Where we wan to be at the end of the insanity
+	int numEpisodes = 0;
+	int goal = 0.8;		// Where we want to be at the end of this insanity
 	int numRows = 15; 	// velocity: 	steps of 0.01
 	int numCols = 41; 	// position: 	steps of 0.05
-	Matrix a[3];		// actions:		forward = 1, coast = 0, reverse = -1
 	
-	double vel = 0.0;	// velocity: 	steps of 0.01
-	double pos = 0.0;	// position: 	steps of 0.05
+	double currVel = 0.0, currPos = 0.0; 	// current state
+	double newVel = 0.0, newPos = 0.0;		// new state
+	double action = 0; 	// actions:	forward = 1, coast = 0, reverse = -1
 	
-	// The third dimension: actions. Thanks matt. 
-	a[0] = new Matrix(numRows, numCols, "reverse");
-	a[1] = new Matrix(numRows, numCols, "coast");	
-	a[2] = new Matrix(numRows, numCols, "forward");
-	
-	for( Matrix &mat : a ) // Initialize Q-matricies to -1, 0, or 1
+	r[0] = new Matrix(numRows, numCols, "reverse");
+	r[1] = new Matrix(numRows, numCols, "coast");	
+	r[2] = new Matrix(numRows, numCols, "forward");
+	for( Matrix &mat : r ) // Initialize state to -1, 0, or 1
 		mat.randInt(-1, 1);
+	
+	q[0] = new Matrix(numRows, numCols, "reverse");
+	q[1] = new Matrix(numRows, numCols, "coast");	
+	q[2] = new Matrix(numRows, numCols, "forward");	
+	for( Matrix &mat : q )
+		mat.constant(0.0); // zero out path matrix
+	
+	
 	
 	
 
 	// Output: 3 Q-matricies to "run a mountain car"
-	for( Matrix &mat : a )
+	for( Matrix &mat : q )
 		mat.write();
 	
 	return(0);
 }
+
+int next( double pos, double vel, int action )
+{
+	
+}
+
 
 // vt+1 = bound(vt + 0.001at − 0.0025 cos(3xt))
 double v( double currVel, double action, double currPos )
