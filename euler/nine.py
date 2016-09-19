9526d2683e69a9ae879ab3942ec517ee2a110dd6
Christopher Goes <ghostofgoes@gmail.com>
2016-09-18 22:37:03 -0700

    This works. Was using a break, after talking to friend, I realized was breaking out of the innermost loop only...yeah.

---
+++ euler/nine.py
@@ -0,0 +1,11 @@
+
+def find_abc():
+    for a in range(1, 500):
+        for b in range(a + 1, 500):
+            for c in range(b + 1, 500):
+                if(a ** 2 + b ** 2 == c ** 2) and (a + b + c == 1000):
+                    return a, b, c
+r = find_abc()
+print(r)
+print(r[0] + r[1] + r[2])
+print(r[0] * r[1] * r[2])
